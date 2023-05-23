9af74cc8b8d804b432e3b7093ffdaab2aba38f63
PrIIIcKoLyAZZZ <367898@edu.itmo.ru>
2023-05-23 22:33:52 +0300

    added pause menu

---
+++ msk2024/Assets/Client/Scripts/PauseMenu/Pause.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] public int _level;
+    [SerializeField] public GameObject _pauseMenuUI;
+    private static bool _isGamePaused = false;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isGamePaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    public void pause()
+    {
+        _pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _isGamePaused = true;
+    }
+
+    public void resume()
+    {
+        _pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _isGamePaused = false;
+    }
+
+    public void restart()
+    {
+        Time.timeScale = 1f;
+        Application.LoadLevel(_level);
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        //TODO 
+    }
+}
